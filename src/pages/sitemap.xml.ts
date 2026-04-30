604a0c969d441e3e3c345facea094932870b64c5
Kevin Bodin <github-actions@users.noreply.github.com>
2026-04-30 10:44:59 +0000

    Add cicatrisation SEO article

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,6 +1,6 @@
 import type { APIRoute } from 'astro';
 import { url } from '../data';
-const pages = ['', 'technique-mixte-sourcils/', 'maquillage-semi-permanent-sourcils-tours/', 'difference-microblading-microshading-technique-mixte/', 'prix-microblading-tours/', 'deroulement-seance/', 'cicatrisation-conseils/', 'tarifs/', 'galerie/', 'demande-avis-sourcils/', 'sophie-bodin/', 'hygiene-securite/', 'technique-mixte-sourcils-pres-de-tours/', 'contact/', 'mentions-legales/', 'confidentialite/'];
+const pages = ['', 'technique-mixte-sourcils/', 'maquillage-semi-permanent-sourcils-tours/', 'difference-microblading-microshading-technique-mixte/', 'prix-microblading-tours/', 'cicatrisation-microblading-conseils/', 'deroulement-seance/', 'cicatrisation-conseils/', 'tarifs/', 'galerie/', 'demande-avis-sourcils/', 'sophie-bodin/', 'hygiene-securite/', 'technique-mixte-sourcils-pres-de-tours/', 'contact/', 'mentions-legales/', 'confidentialite/'];
 export const GET: APIRoute = ({ site }) => {
   const origin = site?.toString().replace(/\/$/, '') || 'https://graindebeaute37.fr';
   const urls = pages.map(p => `<url><loc>${new URL(url(p), origin).toString()}</loc></url>`).join('');
